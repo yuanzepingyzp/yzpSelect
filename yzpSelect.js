43b10d2f9db07ee59efe4d25c72517f6237be8c1
yuanzepingyzp <2808551527@qq.com>
2016-12-19 15:51:54 +0800

    Create yzpSelect.js

---
+++ yzpSelect.js
@@ -0,0 +1,32 @@
+var yzpSelect=angular.module("yzpSelect",[]);
+yzpSelect.directive("yzpSelect",function(){
+	return{
+		scope:{
+			selectName:'@',
+			selectList:'=',
+			yzpModel:'=',
+			placeholder:'@'
+		},
+		restrict:'E',
+		template:'<div><div><input ng-click=toggleSelect() ng-model="yzpModel" placeholder={{placeholder}} readOnly></input></div><select-ul ng-show="isShow" ng-init="isShow=false"></select-ul></div>',
+		link:function(scope){
+			scope.toggleSelect=function(){
+				scope.isShow=!scope.isShow;
+			};
+		}
+	}
+});
+
+yzpSelect.directive("selectUl",function(){
+	return{
+		restrict:'E',
+		replace:'true',
+		template:'<ul><li ng-repeat="item in selectList" ng-click="selected(item)">{{item}}</li></ul>',
+		link:function(scope){
+			scope.selected=function(item){
+				scope.yzpModel=item;
+				scope.toggleSelect();
+			}
+		}
+	}
+});
